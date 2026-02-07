503f13b3b164364b873248948e1e79b93405a4df
Daniël Terwiel <daniel@terwiel.io>
2026-02-07 13:08:32 +0100

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: {
@@ -13,6 +14,9 @@ export default defineConfig({
   plugins: [
     tanstackStart({
       srcDirectory: "app",
+    }),
+    nitro({
+      preset: "vercel",
     }),
     react(),
     tailwindcss(),
